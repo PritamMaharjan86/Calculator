94e329acb8ea9f0bf3984619ff7941c623144da9
Primgdev <primgdev@gmail.com>
2023-09-26 20:20:16 +1000

    added buttons

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="calculator">
+        <span>(0)</span> 0
+      </div>
+          <div className="operators">
+            <button>/</button>
+            <button>*</button>
+            <button>+</button>
+            <button>-</button>
+            <button>DEL</button>
+
+
+          </div>
+
+    </div>
+  );
+}
+
+export default App;
