28ef2557295d5e184b05f900882303049a75888e
Pritam Maharjan <primgdev@gmail.com>
2023-09-27 08:34:17 +1000

    numbers not working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 
 
 function App() {
+
+  const createDigits = ()=>
+  {
+    const digits =[];
+
+    for(let i = 1; i < 10; i++)
+    {
+      digits.push(
+        <button key={i}>{i}</button>
+      )
+      return digits;
+    }
+  }
+
+
   return (
     <div className="App">
       <div className="calculator">
@@ -13,6 +28,15 @@ function App() {
             <button>-</button>
             <button>DEL</button>
 
+
+          </div>
+          <div className="digits">
+            { createDigits() }
+           
+
+            <button>0</button>
+            <button>.</button>
+            <button>=</button>
 
           </div>
 
