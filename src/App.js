09970cff8562daf6ccc9cff7e19c354afb00e803
Primgdev <primgdev@gmail.com>
2023-09-27 10:41:45 +1000

    number fixed

--- src/App.js
+++ src/App.js
@@ -1,47 +1,41 @@
+function App()
+{
+const createDigits = () =>
+{
+  const digits = [];
 
-
-function App() {
-
-  const createDigits = ()=>
+  for (let i = 1; i <10; i++)
   {
-    const digits =[];
-
-    for(let i = 1; i < 10; i++)
-    {
-      digits.push(
-        <button key={i}>{i}</button>
-      )
-      return digits;
-    }
+    digits.push(
+      <button key={i}>{i}</button>
+    )
   }
+  return digits;
+}
 
-
-  return (
+  return(
     <div className="App">
       <div className="calculator">
-        <span>(0)</span> 0
+        <div className="display">
+          <span>(0)</span> 0 
+        </div>
+        <div className="operators">
+          <button>+</button>
+          <button>-</button>
+          <button>*</button>
+          <button>/</button>
+          <button>DEL</button>
+        </div>
+        
+        <div className="digits">
+          {createDigits()}
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
       </div>
-          <div className="operators">
-            <button>/</button>
-            <button>*</button>
-            <button>+</button>
-            <button>-</button>
-            <button>DEL</button>
-
-
-          </div>
-          <div className="digits">
-            { createDigits() }
-           
-
-            <button>0</button>
-            <button>.</button>
-            <button>=</button>
-
-          </div>
-
     </div>
-  );
+  )
 }
 
 export default App;
